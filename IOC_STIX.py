1bdf79bed15ffdff3d4a549aef9c3a7a495e32f3
Lauren Rudman <lauren.rudman101@gmail.com>
2016-02-13 13:46:51 +0200

    Added Report to package

--- IOC_STIX.py
+++ IOC_STIX.py
@@ -1,36 +1,39 @@
-import os, csv, dpkt
+import os, csv, dpkt, datetime
 
 from lib.cuckoo.common.abstracts import Report
 from lib.cuckoo.common.exceptions import CuckooReportError
 
 # python-stix
 import stix.utils as utils
-from stix.core import STIXPackage, STIXHeader
+from stix.core import STIXPackage, STIXHeader 
 from stix.indicator import Indicator
+from stix.report import Header #Report maybe this fails?
+from stix.report import Report as stixReport
+from stix.common import InformationSource
 
+# python-cybox
+# DNS
 from cybox.core import *
 from cybox.objects.dns_query_object import DNSQuery, DNSQuestion, DNSResourceRecords
 from cybox.objects.dns_record_object import DNSRecord
 from cybox.objects.domain_name_object import DomainName
-import datetime
 
+# HTTP
 from cybox.objects.http_session_object import *
 from cybox.objects.uri_object import URI
 
+# Network Connection
 from cybox.objects.network_connection_object import NetworkConnection, Layer7Connections
 from cybox.objects.socket_address_object import SocketAddress
 from cybox.objects.port_object import Port
 
+# IP Address etc
 from cybox.objects.address_object import Address
-
-# python-stix
-import stix.utils as utils
-from stix.core import STIXPackage, STIXHeader
-from stix.indicator import Indicator
 
 class IOC_STIX(Report):
 	def run(self, results):		
 		try:
+			print "Start of IOC_STIX new one"
            		#Do things
 			pcapFile = dpkt.pcap.Reader(file(self.analysis_path+"/cut-byprocessingmodule.pcap"))
 			postDataArray = getPostData(self.analysis_path)
@@ -38,6 +41,7 @@ class IOC_STIX(Report):
 			synConn = getSYNInfo(self.analysis_path)
 			resolvedIPsArray = resolvedIPs(self.analysis_path)
 			if postDataArray != []  or getDomainsArray != [] or synConn != []:
+				print "gatherIOCs soon !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
 				gatherIOCs(self.analysis_path, postDataArray, getDomainsArray, synConn, resolvedIPsArray)
 			else:
 				print "No IOCs to create"
@@ -55,13 +59,13 @@ def getPostData(folderPath):
 		summaryCSV = csv.reader(csvfile, delimiter=',')
 		for row in summaryCSV:
 			if row != []:
-				#print "getPostData", row, type(row)
+				print "getPostData", row, type(row)
 				postDataArray.append(row[0])
 	return postDataArray # array of http request URIs
 
 def getDomains(folderPath): # returns array or domain names
 	#folderNum = folderPath[len(folderPath)-2]
-	#print "get domains",folderNum
+	print "get domains",folderPath
 	os.system("tshark -r "+folderPath+"/cut-byprocessingmodule.pcap -T fields -e dns.qry.name -E separator=, > "+folderPath+"/domains-SUS.csv")
 	urlArray = []
 	# ...
@@ -74,7 +78,7 @@ def getDomains(folderPath): # returns array or domain names
 
 def getSYNInfo(folderPath): 	# writes to a file the pairs of IPs from each SYN connection and the ports
 	#folderNum = folderPath[len(folderPath)-2]
-	#print "getSYNInfo",folderNum
+	print "getSYNInfo",folderPath
 	os.system("tshark -r "+folderPath+"/cut-byprocessingmodule.pcap -w "+folderPath+"/TCPSYN.pcap -F pcap -Y 'tcp.flags.syn==1 and tcp.flags.ack==0'")
 	os.system("tshark -r "+folderPath+"/TCPSYN.pcap -T fields -e ip.src -e ip.dst -e tcp.srcport -e tcp.dstport -E separator=, > "+folderPath+"/SYNConn-SUS.csv")
 	dstIPArray = []
@@ -89,7 +93,7 @@ def getSYNInfo(folderPath): 	# writes to a file the pairs of IPs from each SYN c
 
 def resolvedIPs(folderPath):
 	#folderNum = folderPath[len(folderPath)-2]
-	#print "resolvedIPs", folderNum
+	print "resolvedIPs", folderPath
 	os.system("tshark -r "+folderPath+"/cut-byprocessingmodule.pcap -T fields -e dns.a -E separator=, > "+folderPath+"/domains-SUS.csv")
 	susResolvedIPArray = []
 	# ...
@@ -193,15 +197,22 @@ def removeDuplicates(seq):
     	return [ x for x in seq if not (x in seen or seen_add(x))]
 
 def gatherIOCs(folderPath, postDataArray, getDomains, synConn, resolvedIPs):
+	print "Gather IPs"
 	stix_package = STIXPackage()
-	stix_header = STIXHeader()
-    	stix_header.description = "Dridex domains"
-   	stix_package.stix_header = stix_header		
+	stix_report = stixReport() 	# need to add indicator references to this
+	stix_header_information_source = InformationSource()
+	stix_header_information_source.description = "From Cuckoo sandbox IOC_STIX reporting module"	
+	stix_report.header = Header()
+	stix_report.header.title = "A bunch of related indicators"
+	stix_report.header.short_description = "A short description for the indicators oooooh!"
+	stix_report.header.information_source = stix_header_information_source
+		
 	uris = []
 	tcpSYNips = []
 	tcpSYNports = {}
 	for susip in resolvedIPs:
 		stix_package.add(susIP(susip))
+		stix_report.add_indicator(Indicator(idref=susIP(susip)._id))
 	for info in synConn:
 		tcpSYNips.append(info[1])
 		if info[1] not in tcpSYNports.keys():
@@ -212,13 +223,16 @@ def gatherIOCs(folderPath, postDataArray, getDomains, synConn, resolvedIPs):
 	tcpSYNips = removeDuplicates(tcpSYNips)
 	for z in tcpSYNips:		
 		stix_package.add(TCPSYNobj(z,tcpSYNports[z]))
+		stix_report.add_indicator(Indicator(idref=TCPSYNobj(z,tcpSYNports[z])._id))
 	#print postDataArray
 	xx = removeDuplicates(postDataArray)		
 	for i in xx:
 		stix_package.add(URIobj(i))
+		stix_report.add_indicator(Indicator(idref=URIobj(i)._id))
 	for dd in removeDuplicates(getDomains):
 		stix_package.add(domainNameobj(dd))
-				
+		stix_report.add_indicator(Indicator(idref=domainNameobj(dd)._id))
+	stix_package.add_report(stix_report)		
 	IOCStix = open(folderPath+"/IOCStix.xml",'w')
 	IOCStix.write(stix_package.to_xml())
 	IOCStix.close()	
