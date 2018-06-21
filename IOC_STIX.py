49350688013143fc684b08b42da8396f31cde84f
Lauren <7831472+n0rr1s@users.noreply.github.com>
2018-06-21 14:36:00 +0200

    Update IOC_STIX.py

--- IOC_STIX.py
+++ IOC_STIX.py
@@ -14,14 +14,6 @@ from stix.report import Header
 from stix.report import Report as stixReport
 from stix.common import InformationSource
 
-# TODO http://stixproject.github.io/documentation/idioms/snort-test-mechanism/
-# The test_mechanism is part of the indicator
-
-# TODO add UPD, add record type of DNS query, add whole http request
-
-# python-cybox
-# http://cyboxproject.github.io/documentation/objects/
-
 # DNS
 from cybox.core import *
 from cybox.objects.dns_query_object import DNSQuery, DNSQuestion, DNSResourceRecords
@@ -41,30 +33,28 @@ from cybox.objects.port_object import Port
 from cybox.objects.address_object import Address
 
 # change this to the IP of the virtual machine
-VMIP = "1.1.1.1"
+VMIP = "192.168.0.106"
 
 class IOC_STIX(Report):
-	def run(self, results):		
+	def run(self, results):
 		try:
-			#print "Start of IOC_STIX reporting module\n"+results
-			#pcapFile = dpkt.pcap.Reader(file(self.analysis_path+"/cut-byprocessingmodule.pcap"))
+			dnspacket = getDNSData(self.analysis_path)
 			goodIPs = getMicrosoftDomains(self.analysis_path)
 			synConn = getSYNInfo(self.analysis_path, goodIPs)
 			synackconn = getSYNACKInfo(self.analysis_path, goodIPs)
 			ackConn = getACKInfo(self.analysis_path, goodIPs)
 			resolvedIPsArray = resolvedIPs(self.analysis_path, goodIPs)
-			fullHTTPArray = getFullHTTP(self.analysis_path)
+			fullHTTPArray = getFullHTTP(self.analysis_path, dnspacket)
 			udpconn = getUDPData(self.analysis_path, goodIPs)
-			dnspacket = getDNSData(self.analysis_path)
 			icmpPacket = getICMPData(self.analysis_path)
 			ftpconn = getFTPConn(self.analysis_path)
 			sshconn = getSSHConn(self.analysis_path)
-			foundIPs = findStaticIPs(results["strings"])			
+			foundIPs = findStaticIPs(results["strings"])
 			if synConn!=[] or synackconn!=[] or ackConn!=[] or resolvedIPsArray!=[] or fullHTTPArray!=[] or udpconn!=[] or dnspacket!=[] or icmpPacket!=[] or ftpconn!=[] or sshconn!=[] or foundIPs!=[]:
 				gatherIOCs(self.analysis_path, synConn, synackconn, ackConn, resolvedIPsArray, results, fullHTTPArray, udpconn, dnspacket, icmpPacket, ftpconn, sshconn, foundIPs)
 			else:
 				print "No IOCs to create"
-			
+
         	except (UnicodeError, TypeError, IOError) as e:
 			print "Error", e
             		raise CuckooReportError("Failed to make STIX IOCs :(")
@@ -78,25 +68,18 @@ def findStaticIPs(stringlist):
 		if i != []:
 			if valid_ip(i):
 				try:
-					#print "Before socket: ", i
 					socket.inet_aton(getIP(i))
 					arrayofIPs.append(getIP(i))
-					print "Found static: ", i
 				except:
-					print "Not a static IP", i
+					continue
 	return arrayofIPs
 
 
 def valid_ip(address):
 	regex = re.findall(r"\b(?:[0-9]{1,3}\.){3}[0-9]{1,3}\b", address)
-	# https://www.safaribooksonline.com/library/view/regular-expressions-cookbook/9780596802837/ch07s16.html
-	#print "May be wrong, Found:      -------------------- >         ", address, regex	
-	#print "Regex", regex
 	if regex != []:
-		#print "Found in IP", regex
 		return True
 	else:
-		#print "Not IP", regex
 		return False
 
 def getIP(string):
@@ -109,19 +92,16 @@ def getMicrosoftDomains(folderPath):
 		summaryCSV = csv.reader(csvfile, delimiter=',')
 		for row in summaryCSV:			
 			if row!=[]:
-				if row[0].endswith("microsoft.com") or row[0].endswith("windowsupdate.com") or row[0].endswith("trafficmanager.net") or row[0].endswith("msocsp.com") or row[0].endswith("gvt1.com") or row[0].endswith("verisign.com") or row[0].endswith("windows.com") or row[0].endswith("google.com"): # to be extra sure no good traffic gets through
-					#print "Row after", row
+				if row[0].endswith("microsoft.com") or row[0].endswith("windowsupdate.com") or row[0].endswith("trafficmanager.net") or row[0].endswith("msocsp.com") or row[0].endswith("gvt1.com") or row[0].endswith("verisign.com") or row[0].endswith("windows.com") or row[0].endswith("google.com"):
 					for ips in row:
 						if valid_ip(ips) and ips not in niceIPs:
 							niceIPs.append(ips)
-	#print "Good IPs", niceIPs
 	return niceIPs    
 
 # SSH
 # source IP, source port, destination address, destination port
 # https://www.wireshark.org/docs/dfref/s/ssh.html
 def getSSHConn(folderPath):
-	#print "getSSHConn"
 	os.system('tshark -r '+folderPath+'/cut-byprocessingmodule.pcap -w '+folderPath+'/SSHpackets.pcap -F pcap -Y ssh -T fields -e ip.src -e tcp.srcport -e ip.dst -e tcp.dstport -e tcp.flags.syn -e tcp.flags.ack -e ssh.kexdh.host_key -E separator=, > '+folderPath+'/SSHInfo.csv')
 	sshpacket = []
 	with open(folderPath+"/SSHInfo.csv", 'rb') as csvfile:
@@ -134,7 +114,6 @@ def getSSHConn(folderPath):
 # FTP
 # source IP, source port, destination address, destination port
 def getFTPConn(folderPath):
-	#print "getFTPConn"
 	os.system('tshark -r '+folderPath+'/cut-byprocessingmodule.pcap -w '+folderPath+'/FTPpackets.pcap -F pcap -Y ftp -T fields -e ip.src -e tcp.srcport -e ip.dst -e tcp.dstport -e ftp.response.code -e ftp.request.command -e ftp.request.arg -e ftp.response.arg -E separator=, > '+folderPath+'/FTPInfo.csv')
 	FTPpacket = []
 	with open(folderPath+"/FTPInfo.csv", 'rb') as csvfile:
@@ -147,7 +126,6 @@ def getFTPConn(folderPath):
 # ICMP
 # source and destination address, type 
 def getICMPData(folderPath):
-	#print "getICMPData"
 	os.system('tshark -r '+folderPath+'/cut-byprocessingmodule.pcap -w '+folderPath+'/icmppackets.pcap -F pcap -Y icmp -T fields -e icmp.type -e ip.src -e ip.dst -E separator=, > '+folderPath+'/ICMPInfo.csv')
 	ICMPpacket = []
 	with open(folderPath+"/ICMPInfo.csv", 'rb') as csvfile:
@@ -161,7 +139,6 @@ def getICMPData(folderPath):
 # source port, destination port, destination ip
 # https://www.wireshark.org/docs/dfref/u/udp.html
 def getUDPData(folderPath, goodIPs):
-	#print "getUDPData"
 	os.system('tshark -r '+folderPath+'/cut-byprocessingmodule.pcap -w '+folderPath+'/UDPpackets.pcap -F pcap -Y udp -T fields -e udp.srcport -e udp.dstport -e ip.dst -e ip.src -E separator=, > '+folderPath+'/UDPInfo.csv')
 	udppacket = []
 	with open(folderPath+"/UDPInfo.csv", 'rb') as csvfile:
@@ -172,21 +149,17 @@ def getUDPData(folderPath, goodIPs):
 	return udppacket
 
 def getDNSData(folderPath):
-	#print "getDNSData"
 	os.system('tshark -r'+folderPath+'/cut-byprocessingmodule.pcap -w '+folderPath+'/DNSpackets.pcap -F pcap -Y dns.flags.response==0 -T fields -e ip.src -e udp.srcport -e ip.dst -e udp.dstport -e dns.qry.name -e dns.qry.type -e dns.flags.response -E separator=~ > '+folderPath+'/DNSInfo.csv')
-	# -e dns.resp.name -e dns.resp.ttl -e dns.resp.type -e dns.a -e dns.flags.response -E separator=~ > '+folderPath+'/DNSInfo.csv')
 	dnspacket = []
 	with open(folderPath+"/DNSInfo.csv", 'rb') as csvfile:
 		summaryCSVDNS = csv.reader(csvfile, delimiter='~')
 		for row in summaryCSVDNS:
-			print 'row[4].endswith("microsoft.com")', row[4].endswith("microsoft.com"), row[4]
 			if row != [] and row not in dnspacket and not row[4].endswith("microsoft.com") and not row[4].endswith("windowsupdate.com") and not row[4].endswith("gvt1.com") and not row[4].endswith("google.com"):
 				dnspacket.append(row)
 	return dnspacket
 
 
-def getFullHTTP(folderPath):
-	#print "getFullHTTP"
+def getFullHTTP(folderPath, dnspacket):
 	comm = 'tshark -r '+folderPath+'/cut-byprocessingmodule.pcap -w '+folderPath+'/HTTPGETpackets.pcap -F pcap -Y http.request.method=="GET" -T fields -e http.request.method -e http.request.uri -e http.request.version -e tcp.dstport -e http.accept -e http.accept_language -e http.accept_encoding -e http.authorization -e http.cache_control -e http.connection -e http.cookie -e http.content_length -e http.content_type -e http.date -e http.host -e http.proxy_authorization -E separator=~ > '+folderPath+'/HTTPFullGET.csv'	
 	os.system(comm)
 	comm2 = 'tshark -r '+folderPath+'/cut-byprocessingmodule.pcap -w '+folderPath+'/HTTPPOSTpackets.pcap -F pcap -Y http.request.method=="POST" -T fields -e http.request.method -e http.request.uri -e http.request.version -e tcp.dstport -e http.accept -e http.accept_language -e http.accept_encoding -e http.authorization -e http.cache_control -e http.connection -e http.cookie -e http.content_length -e http.content_type -e http.date -e http.host -e http.proxy_authorization -E separator=~ > '+folderPath+'/HTTPFullPOST.csv'		
@@ -206,7 +179,6 @@ def getFullHTTP(folderPath):
 
 
 def getSYNInfo(folderPath, goodIPs):
-	#print "getSYNInfo"
 	os.system("tshark -r "+folderPath+"/cut-byprocessingmodule.pcap -w "+folderPath+"/TCPSYN.pcap -F pcap -Y 'tcp.flags.syn==1 and tcp.flags.ack==0 and tcp.flags.cwr==0 and tcp.flags.ecn==0 and tcp.flags.fin==0 and tcp.flags.ns==0 and tcp.flags.push==0 and tcp.flags.res==0 and tcp.flags.reset==0 and tcp.flags.urg==0' -T fields -e ip.src -e ip.dst -e tcp.srcport -e tcp.dstport -E separator=, > "+folderPath+"/SYNConn.csv")
 	dstIPArray = []
 	with open(folderPath+"/SYNConn.csv", 'rb') as csvfile:
@@ -215,11 +187,9 @@ def getSYNInfo(folderPath, goodIPs):
 			print row
 			if (tuple(row) not in dstIPArray) and row[0] not in goodIPs and row[1] not in goodIPs:
 				dstIPArray.append(tuple(row))
-	#print "SYN", dstIPArray
 	return dstIPArray
 
-def getSYNACKInfo(folderPath, goodIPs):	
-	#print "getSYNACKInfo"
+def getSYNACKInfo(folderPath, goodIPs):
 	os.system("tshark -r "+folderPath+"/cut-byprocessingmodule.pcap -w "+folderPath+"/TCPSYNACK.pcap -F pcap -Y 'tcp.flags.syn==1 and tcp.flags.ack==1 and tcp.flags.cwr==0 and tcp.flags.ecn==0 and tcp.flags.fin==0 and tcp.flags.ns==0 and tcp.flags.push==0 and tcp.flags.res==0 and tcp.flags.reset==0 and tcp.flags.urg==0' -T fields -e ip.dst -e ip.src -e tcp.dstport -e tcp.srcport -E separator=, > "+folderPath+"/SYNACKConn.csv")	
 	dstIPArray = []
 	with open(folderPath+"/SYNACKConn.csv", 'rb') as csvfile:
@@ -227,11 +197,9 @@ def getSYNACKInfo(folderPath, goodIPs):
 		for row in summaryCSV:
 			if (tuple(row) not in dstIPArray) and row[0] not in goodIPs and row[1] not in goodIPs:
 				dstIPArray.append(tuple(row))
-	#print "SYN-ACK", dstIPArray
 	return dstIPArray
 
 def getACKInfo(folderPath, goodIPs):
-	#print "getACKInfo"
 	os.system("tshark -r "+folderPath+"/cut-byprocessingmodule.pcap -w "+folderPath+"/TCPACK.pcap -F pcap -Y 'tcp.flags.syn==0 and tcp.flags.ack==1 and tcp.flags.cwr==0 and tcp.flags.ecn==0 and tcp.flags.fin==0 and tcp.flags.ns==0 and tcp.flags.push==0 and tcp.flags.res==0 and tcp.flags.reset==0 and tcp.flags.urg==0' -T fields -e ip.src -e ip.dst -e tcp.srcport -e tcp.dstport -E separator=, > "+folderPath+"/ACKConn.csv")
 	dstIPArray = []
 	with open(folderPath+"/ACKConn.csv", 'rb') as csvfile:
@@ -239,12 +207,9 @@ def getACKInfo(folderPath, goodIPs):
 		for row in summaryCSV:
 			if (tuple(row) not in dstIPArray) and row[0] not in goodIPs and row[1] not in goodIPs:
 				dstIPArray.append(tuple(row))
-	#print "ACK", dstIPArray
 	return dstIPArray
 
 def resolvedIPs(folderPath, goodIPs):
-	#print "resolvedIPs"
-	
 	os.system("tshark -r "+folderPath+"/cut-byprocessingmodule.pcap -Y dns.flags.response==1 -T fields -e dns.qry.name -e dns.a -E separator=, > "+folderPath+"/domains-SUS-IPs.csv")
 	susResolvedIPArray = []
 	with open(folderPath+"/domains-SUS-IPs.csv", 'rb') as csvfile:
@@ -253,14 +218,11 @@ def resolvedIPs(folderPath, goodIPs):
 			if row != []:
 				if not row[0].endswith("microsoft.com") and not row[0].endswith("windowsupdate.com"):
 					for i in row:
-						#print "Resolved IPs", i
-						if valid_ip(i) and i not in goodIPs:				
-							#print "IP: -> ", i
+						if valid_ip(i) and i not in goodIPs:
 							susResolvedIPArray.append(i)
 	return removeDuplicates(susResolvedIPArray)
 
 def domainNameobj(domain):
-	#print "DomainObj"
 	# cybox stuff
 	d = DomainName()
 	d.value = domain
@@ -272,18 +234,7 @@ def domainNameobj(domain):
 	indicator1.add_object(d)
 	return indicator1
 
-#def URIobj(httpR):
-#	h = URI()
-#	h.value = str(httpR) 
-#	indicator = Indicator()
-#   	indicator.title = "URI"
-#    	indicator.description = ("An indicator containing a suspicious URI")
-#	indicator.set_produced_time(utils.dates.now())
-#	indicator.add_object(h)
-#	return indicator
-
 def HTTPFullObj(http):
-	#print "HTTPFullObj"
 	httprequestline = HTTPRequestLine()
 	httprequestline.http_method = http[0]
 	httprequestline.value = http[1]
@@ -330,8 +281,7 @@ def HTTPFullObj(http):
 	http_request_response.http_client_request = httpclientrequest
 	
 	httpsession = HTTPSession()	
-	httpsession.http_request_response = http_request_response	
-	#print "Still in http obj, almost done"
+	httpsession.http_request_response = http_request_response
 	layer7connections = Layer7Connections()
 	layer7connections.http_session = httpsession
 	networkconnection = NetworkConnection()
@@ -348,7 +298,6 @@ def HTTPFullObj(http):
 
 # source IP, source port, destination address, destination port
 def TCPConnectionAttemptFailedObj(tcpinfo):
-	#print "TCPConnectionAttemptFailedObj"
 	networkconnection = NetworkConnection()
 	networkconnection.layer3_protocol = "IPv4"
 	networkconnection.layer4_protocol = "TCP"	
@@ -379,7 +328,6 @@ def TCPConnectionAttemptFailedObj(tcpinfo):
 
 # source IP, source port, destination address, destination port
 def TCPConnectionEstablishedObj(tcpinfo):
-	#print "TCPConnectionEstablishedObj"
 	networkconnection = NetworkConnection()
 	networkconnection.layer3_protocol = "IPv4"
 	networkconnection.layer4_protocol = "TCP"
@@ -410,8 +358,6 @@ def TCPConnectionEstablishedObj(tcpinfo):
 
 # source port, destination port, destination ip, source ip
 def UDPRequestObj(udpinfo):
-	#print "UDPRequestObj"
-	#print "In UDP Object"
 	u = NetworkConnection()
 	u.layer3_protocol = "IPv4"
 	u.layer4_protocol = "UDP"
@@ -441,8 +387,7 @@ def UDPRequestObj(udpinfo):
 
 
 #-e ip.src -e udp.srcport -e ip.dst -e udp.dstport -e dns.qry.name -e dns.qry.type -e dns.flags.response	
-def DNSRequestObj(dnsinfo): 
-	#print "DNSRequestObj"
+def DNSRequestObj(dnsinfo):
 	networkconnection = NetworkConnection()
 	networkconnection.layer3_protocol = "IPv4"
 	networkconnection.layer4_protocol = "UDP"
@@ -477,7 +422,6 @@ def DNSRequestObj(dnsinfo):
 
 # type, source address, destination address
 def ICMPObj(icmp):
-	#print "ICMPObj"
 	# block types 0 (ping response), 8 (ping request)
 	nc = NetworkConnection()
 	indicator = Indicator()
@@ -515,7 +459,6 @@ def ICMPObj(icmp):
 # ip.src, tcp.srcport, ip.dst, tcp.dstport, ftp.response.code, ftp.request.command, ftp.request.arg, ftp.response.arg
 # https://en.wikipedia.org/wiki/List_of_FTP_commands
 def FTPObj(ftp):
-	#print "FTPObj"
 	networkconnection = NetworkConnection()
 	networkconnection.layer3_protocol = "IPv4"
 	networkconnection.layer4_protocol = "TCP"
@@ -680,7 +623,6 @@ def FTPObj(ftp):
 
 # source IP, source port, destination address, destination port
 def SSHObj(SSH):
-	#print "SSHObj"
 	networkconnection = NetworkConnection()
 	networkconnection.layer3_protocol = "IPv4"
 	networkconnection.layer4_protocol = "TCP"
@@ -716,8 +658,6 @@ def SMTP(smtpinfo):
 	pass
 
 def susIP(ip):
-	#print "SUSIP"
-	#TODO (from dns response) 
 	a = Address()
 	a.address_value = ip
 	a.category = a.CAT_IPV4
@@ -729,8 +669,6 @@ def susIP(ip):
 	return indicator
 
 def susIPfound(ip):
-	#print "SUSIP----found"
-	#TODO (from dns response) 
 	a = Address()
 	a.address_value = ip
 	a.category = a.CAT_IPV4
@@ -742,18 +680,15 @@ def susIPfound(ip):
 	return indicator
 
 def removeDuplicates(seq):
-	#print "removeDuplicates"
 	seen = set()
     	seen_add = seen.add
     	return [ x for x in seq if not (x in seen or seen_add(x))]
 
 def translateType(typeNumber):
-	#print "translateType"
 	typeDict = {'1':'A', '2':'NS', '5':'CNAME', '15':'MX', '6':'SOA', '16':'TXT', '28':'AAAA'}
 	return typeDict[typeNumber]
 		 
 def gatherIOCs(folderPath, synConn, synackConn, ackConn, resolvedIPs, results, fullHTTPArray, udpconn, dnspacket, icmpPacket, ftpconn, sshconn, foundIPs):
-	#print "Gather IOCs"
 	stix_package = STIXPackage()
 	stix_report = stixReport() 	# need to add indicator references to this
 	stix_header_information_source = InformationSource()
@@ -762,80 +697,66 @@ def gatherIOCs(folderPath, synConn, synackConn, ackConn, resolvedIPs, results, f
 	stix_report.header.title = "A bunch of related indicators"
 	stix_report.header.short_description = "A short description for the indicators oooooh!"
 	stix_report.header.information_source = stix_header_information_source
-		
-
 
 # IP address
 	for susip in resolvedIPs:
 		stix_package.add(susIP(susip))
-		stix_report.add_indicator(Indicator(idref=susIP(susip)._id))
+		stix_report.add_indicator(Indicator())
 	
 # IPs found as static strings in the file	
 	for IP in foundIPs:
 		stix_package.add(susIPfound(IP))
-		stix_report.add_indicator(Indicator(idref=susIPfound(IP)._id))
+		stix_report.add_indicator(Indicator())
 
 # TCP Connection attempt and Connection established
 	for tcp in synConn:
-		if tcp not in ackConn:
-			#print "tcp: ", tcp		
+		if tcp not in ackConn:		
 			stix_package.add(TCPConnectionAttemptFailedObj(tcp))
-			stix_report.add_indicator(Indicator(idref=TCPConnectionAttemptFailedObj(tcp)._id))
+			stix_report.add_indicator(Indicator())
 
 	for tcpest in synConn:
-		if tcpest in synackConn and tcpest in ackConn:
-			#print "tcpest: ", tcpest		
+		if tcpest in synackConn and tcpest in ackConn:		
 			stix_package.add(TCPConnectionEstablishedObj(tcpest))
-			stix_report.add_indicator(Indicator(idref=TCPConnectionEstablishedObj(tcpest)._id))
+			stix_report.add_indicator(Indicator())
 
 # Full HTTP Request
 	for ht in fullHTTPArray:
-		#print "ht array part:  ", ht, type(ht)
 		stix_package.add(HTTPFullObj(ht))
-		stix_report.add_indicator(Indicator(idref=HTTPFullObj(ht)._id))
+		stix_report.add_indicator(Indicator())
 
 # UDP Connection
-	#print udpconn, type(udpconn)
 	for udp in udpconn:
 		if udp[0]!='53' and udp[1]!='53': # ignore DNS UDP packets (they are logged else where)
-			#print "udp: ", udp		
 			stix_package.add(UDPRequestObj(udp))
-			stix_report.add_indicator(Indicator(idref=UDPRequestObj(udp)._id))
+			stix_report.add_indicator(Indicator())
 
 # DNS Connection
-	for dns in dnspacket:
-		#print "dns: ", dns		
+	for dns in dnspacket:		
 		stix_package.add(DNSRequestObj(dns))
-		stix_report.add_indicator(Indicator(idref=DNSRequestObj(dns)._id))
+		stix_report.add_indicator(Indicator())
 
 # ICMP Connection
 	for icmp in icmpPacket:
-		#print "ICMP: ", icmp
 		if icmp[0] == 0 or icmp[0] == 8:
 			stix_package.add(ICMPObj(icmp))
-			stix_report.add_indicator(Indicator(idref=ICMPObj(icmp)._id))
+			stix_report.add_indicator(Indicator())
 
 # FTP Connection
 	for ftp in ftpconn:
-		#print "FTP: ", ftp
 		if ftp[4]=='220' or ftp[4]=='230' or ftp[4]=='250':			
 			stix_package.add(FTPObj(ftp))
-			stix_report.add_indicator(Indicator(idref=FTPObj(ftp)._id))
+			stix_report.add_indicator(Indicator())
 		elif ftp[5]=="USER" or ftp[5]=="PASS" or ftp[5]=="STOR" or ftp[5]=="RETR":
 			stix_package.add(FTPObj(ftp))
-			stix_report.add_indicator(Indicator(idref=FTPObj(ftp)._id))
+			stix_report.add_indicator(Indicator())
 			
 
 # SSH Connection
 	for ssh in sshconn:
-		#print "SSH: ", ssh
 		stix_package.add(SSHObj(ssh))
-		stix_report.add_indicator(Indicator(idref=SSHObj(ssh)._id))
+		stix_report.add_indicator(Indicator())
 
-	stix_package.add_report(stix_report)
-	#print results["target"]		
+	stix_package.add_report(stix_report)	
 	IOCStix = open(folderPath+"/"+str(results["target"]["file"]["name"])+".xml",'w')
 	IOCStix.write(stix_package.to_xml())
-	IOCStix.close()	
-	#print(stix_package.to_xml())	
-
+	IOCStix.close()
